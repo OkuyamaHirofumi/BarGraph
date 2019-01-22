bf25c0da2a87e0b0f11dcdc11a8d26ca711b48cb
okuyamahirofumi <toruko1121@gmail.com>
2019-01-22 23:59:01 +0900

    first commit

---
+++ app/src/main/java/com/example/bargraph/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.bargraph;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createBarChart();
+    }
+
+    private void createBarChart() {
+        BarChart barChart = (BarChart) findViewById(R.id.bar_chart);
+        barChart.setDescription("BarChart 説明");
+
+        barChart.getAxisRight().setEnabled(false);
+        barChart.getAxisLeft().setEnabled(true);
+        barChart.setDrawGridBackground(true);
+        barChart.setDrawBarShadow(false);
+        barChart.setEnabled(true);
+
+        barChart.setTouchEnabled(true);
+        barChart.setPinchZoom(true);
+        barChart.setDoubleTapToZoomEnabled(true);
+
+        barChart.setHighlightEnabled(true);
+        barChart.setDrawHighlightArrow(true);
+        barChart.setHighlightEnabled(true);
+
+        barChart.setScaleEnabled(true);
+
+        barChart.getLegend().setEnabled(true);
+
+        //X軸周り
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setDrawLabels(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(true);
+        xAxis.setSpaceBetweenLabels(0);
+
+        barChart.setData(createBarChartData());
+
+        barChart.invalidate();
+        // アニメーション
+        barChart.animateY(2000, Easing.EasingOption.EaseInBack);
+    }
+
+    // BarChartの設定
+    private BarData createBarChartData() {
+        ArrayList<BarDataSet> barDataSets = new ArrayList<>();
+
+        // X軸
+        ArrayList<String> xValues = new ArrayList<>();
+        xValues.add("1月");
+        xValues.add("2月");
+        xValues.add("3月");
+        xValues.add("4月");
+        xValues.add("5月");
+        xValues.add("6月");
+
+        // valueA
+        ArrayList<BarEntry> valuesA = new ArrayList<>();
+        valuesA.add(new BarEntry(100, 0));
+        valuesA.add(new BarEntry(200, 1));
+        valuesA.add(new BarEntry(300, 2));
+        valuesA.add(new BarEntry(400, 3));
+        valuesA.add(new BarEntry(500, 4));
+        valuesA.add(new BarEntry(600, 5));
+
+        BarDataSet valuesADataSet = new BarDataSet(valuesA, "A");
+        valuesADataSet.setColor(ColorTemplate.COLORFUL_COLORS[3]);
+
+        barDataSets.add(valuesADataSet);
+
+        // valueB
+        ArrayList<BarEntry> valuesB = new ArrayList<>();
+        valuesB.add(new BarEntry(200, 0));
+        valuesB.add(new BarEntry(300, 1));
+        valuesB.add(new BarEntry(400, 2));
+        valuesB.add(new BarEntry(500, 3));
+        valuesB.add(new BarEntry(600, 4));
+        valuesB.add(new BarEntry(700, 5));
+
+        BarDataSet valuesBDataSet = new BarDataSet(valuesB, "B");
+        valuesBDataSet.setColor(ColorTemplate.COLORFUL_COLORS[4]);
+
+        barDataSets.add(valuesBDataSet);
+
+        BarData barData = new BarData(xValues, barDataSets);
+        return barData;
+    }
+
+}
